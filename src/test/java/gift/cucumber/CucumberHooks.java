dfacd3bd902e6dca535749eea9d65618351395dc
sapiensxxv <jhspacelover@naver.com>
2026-02-25 10:39:34 +0900

    fix: postgres 도커 이미지 진입 포트 번호 수정

--- src/test/java/gift/cucumber/CucumberHooks.java
+++ src/test/java/gift/cucumber/CucumberHooks.java
@@ -15,7 +15,7 @@ public class CucumberHooks {
         // 컨테이너 App으로 HTTP 요청 (Host → localhost:28080 → Docker App)
         RestAssured.baseURI = "http://localhost:28080";
 
-        // DB 초기화 (Host → localhost:5432 → Docker PostgreSQL)
+        // DB 초기화 (Host → localhost:15432 → Docker PostgreSQL)
         jdbcTemplate.execute("TRUNCATE TABLE option, wish, product, category, member CASCADE");
     }
 }
